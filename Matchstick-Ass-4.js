a8a83eb0f4c2e6133e9b906fd2e4125b5012ec51
Paritosh Chaudhary <51153681+Paritosh008@users.noreply.github.com>
2023-02-03 12:44:11 +0530

    Create Matchstick-Ass-4.js

---
+++ Matchstick-Ass-4.js
@@ -0,0 +1,9 @@
+#This assignment will help you interpret mathematical relationships both algebraically and geometrically.
+
+function countMatchstick(num)
+
+{
+    return (6*num)-(num-1)
+}
+
+console.log (countMatchstick(87));
